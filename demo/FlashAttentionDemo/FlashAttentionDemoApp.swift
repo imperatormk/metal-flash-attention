86a34f54c57d8b48b5f13a3bb014d6eb1741d667
imperatormk <darko.simonovski@hotmail.com>
2026-02-09 02:53:50 +0100

    Add demo app: FlashAttention on macOS + iOS

---
+++ demo/FlashAttentionDemo/FlashAttentionDemoApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct FlashAttentionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
